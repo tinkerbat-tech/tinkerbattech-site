c17418309b509197fa72ba2380543e945c127408
Stephanie Anderson <srglover126@gmail.com>
2025-12-20 21:17:10 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="title-section">
+        <h1 className="title">Tinkerbat Tech</h1>
+        <h2 className="subtitle">
+          Welcome to my spooky little world! I create apps inspired from
+          Victorian times, Steampunk fantasy, and gothic horror. I have mainly
+          created them for myself, but I'd like to share them with you. I hope
+          they bring you joy.{" "}
+        </h2>
+      </div>
+      <div className="landing">
+        <h3 className="app-title">Expense Graveyard</h3>
+        <p className="tagline">Your travel buddy.</p>
+        <button className="cta-button">Coming Soon</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
